e82cf3426f092a7d7ee7764dea5bcd1085c54d5f
Ryan Martin <rlmartin@gmail.com>
2018-04-14 02:52:45 -0400

    Fix create table statements

--- generators/schema-evolution-manager/tables/{{#each enums}}{{toTableName name}}{{:each}}.sql
+++ generators/schema-evolution-manager/tables/{{#each enums}}{{toTableName name}}{{:each}}.sql
@@ -1,7 +1,7 @@
-create table {{toTableName name}} {
+create table {{toTableName name}} (
   key text primary key not null,
   created_at timestamptz not null default now()
-};
+);
 
 {{#each values}}
 insert into {{toTableName ../name}} (key) values ('{{name}}');
--- generators/schema-evolution-manager/tables/{{#each models}}{{toTableName plural}}{{:each}}.sql
+++ generators/schema-evolution-manager/tables/{{#each models}}{{toTableName plural}}{{:each}}.sql
@@ -1,8 +1,8 @@
-create table {{toTableName plural}} {
+create table {{toTableName plural}} (
 {{#each fields}}
   {{#ifReservedWord name "postgres"}}`{{name}}`{{else}}{{toSnakeCase name}}{{/ifReservedWord}} {{columnDefinition . ../_root_.enums}}{{#ifNotLast ../fields @index}},{{/ifNotLast}}
 {{/each}}
-}
+);
 
 {{#each fields}}
 {{#ifEmpty description}}
